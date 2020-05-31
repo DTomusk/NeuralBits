88d25ab7d833c67381f7fb11f901efeb6b1e719b
DTomusk <david.tomusk@gmail.com>
2020-05-31 15:19:13 +0100

    Added feed forward function (with a bunch of .iter()s and .map()s)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod netwerk;
+mod network;
 
 fn main() {
     /*
@@ -6,19 +6,27 @@ fn main() {
     let num = sigmoid(0.0);
     println!("Sigmoid of 420: {}", num);
     */
-    let mut my_network = netwerk::Network::new(vec![3,10,5,10]);
+    let mut my_network = network::Network::new(vec![3,10,5,10]);
 
-    println!("{:#?}", my_network);
+    my_network.feed_forward(vec![0.0, 1.1, 2.2,]);
 
-    let mut data = vec![0.78,-0.53,0.21];
+    //let mut data = vec![0.78,-0.53,0.21];
 }
 
-#[cfg(Test)]
+#[cfg(test)]
 mod tests {
     use super::*;
 
-    // test whether the dimensions of a network are as expected 
+    // test whether the dimensions of a network are as expected
     #[test]
     fn network_dimensions() {
+    }
+
+    // maybe should be separate tests, but testing dot product
+    #[test]
+    fn dot_product() {
+        assert_eq!(network::Network::dot_product(vec![1.0, 2.0], vec![2.0, 3.0]), 8.0);
+        assert_eq!(network::Network::dot_product(vec![5.0, 3.0], vec![-1.0, 3.0]), 4.0);
+        assert_eq!(network::Network::dot_product(vec![5.0, 3.0, 2.0, 1.0], vec![-1.0, 3.0, 4.0]), 12.0);
     }
 }
--- src/network.rs
+++ src/network.rs
@@ -3,6 +3,8 @@ extern crate rand;
 use rand::prelude::*;
 use rand::distributions::StandardNormal;
 
+const E: f64 = 2.71828;
+
 #[derive(Debug)]
 pub struct Network {
     num_layers: usize,
@@ -32,27 +34,27 @@ impl Network {
         for i in 1..self.num_layers {
             let mut current_bias: Vec<f64> = vec![];
             let num = self.sizes[i] as usize;
-            for j in 0..num {
+            for _ in 0..num {
                 current_bias.push(SmallRng::from_entropy().sample(StandardNormal));
             }
             self.biases.push(current_bias);
         }
     }
 
+    // each node has a number of weights equal to the number of nodes in the previous layer
     fn set_weights(&mut self) {
         println!("{:?}", self.sizes);
         let mut w = self.sizes.clone();
         w.remove(0);
         let dim = w.iter().zip(self.sizes.iter());
-        println!("{:?}", dim);
 
         // each element of dim represents the dimensions of an array of weights
         // each tuple in dim is a layer and each layer contains an array
         for (x, y) in dim {
             let mut layer: Vec<Vec<f64>> = vec![];
-            for i in 0..*x {
+            for _ in 0..*x {
                 let mut array: Vec<f64> = vec![];
-                for j in 0..*y {
+                for _ in 0..*y {
                     array.push(SmallRng::from_entropy().sample(StandardNormal));
                 }
                 layer.push(array);
@@ -60,4 +62,31 @@ impl Network {
             self.weights.push(layer);
         }
     }
+
+    // I'm having a lot of fun using .iter() and stuff
+    // here the network is given an input a which is dot producted with every weight for each node, summed with the biases and sigmoided through all the layers
+    pub fn feed_forward(&mut self, mut a: Vec<f64>) -> Vec<f64> {
+        let mut t: Vec<f64> = vec![];
+        for (w, b) in self.weights.iter().zip(self.biases.iter()) {
+            for n in w.iter() {
+                t.push(dot_product(n.to_vec(), a.clone()));
+            }
+            a = sigmoid(t.iter().zip(b.iter()).map(|(x,y)| x + y).collect());
+            t = vec![];
+        }
+        println!("{:#?}", a);
+        a
+    }
+}
+
+fn sigmoid(z: Vec<f64>) -> Vec<f64> {
+    let mut v: Vec<f64> = vec![];
+    for x in z {
+        v.push(1.0 / (1.0 + E.powf(-x)));
+    }
+    v
+}
+
+pub fn dot_product(a: Vec<f64>, b: Vec<f64>) -> f64 {
+    a.iter().zip(b.iter()).map(|(x,y)| x * y).sum()
 }
